24a46782143581f2f31e21a1732c3f170c61ece6
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-10-31 12:25:35 -0700

    refactor

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left)
 	{
 		left_child->left = parent->left;
-		parent->left->parent = left_child;
+		left_child->left->parent = left_child;
 	}
 	parent->left = left_child;
 	return (left_child);
