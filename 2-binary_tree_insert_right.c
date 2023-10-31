ecfb09f72bcc34ff289339baf9472ccaefa6ba62
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-10-31 13:32:55 -0700

    fix error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,23 +12,17 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *right_child;
 
-	right_child = malloc(sizeof(binary_tree_t));
-	if (right_child == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	right_child->n = value;
-	right_child->parent = parent;
-
-	if (parent->right != NULL)
+	right_child = binary_tree_node(parent, value);
+	if (right_child == NULL)
+		return (NULL);
+	if (parent->right)
 	{
 		right_child->right = parent->right;
-		parent->right->parent = right_child;
-		parent->right = right_child;
+		right_child->right->parent = right_child;
 	}
-	else
-	{
-		parent->right = right_child;
-	}
-
+	parent->right = right_child;
 	return (right_child);
 }
