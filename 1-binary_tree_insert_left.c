5190755e2b9e3628ceaed52825d79e37b6a05e8a
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-10-31 13:28:18 -0700

    fix error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	left_child = binary_tree_node(parent, value);
-	if (left_node == NULL)
+	if (left_child == NULL)
 		return (NULL);
 	if (parent->left)
 	{
