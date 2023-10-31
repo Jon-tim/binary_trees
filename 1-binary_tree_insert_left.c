ca915ed949694a206512c4226dedbbf3a6b187d4
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-10-31 12:17:56 -0700

    refactor

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,23 +12,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *left_child;
 
-	left_child = malloc(sizeof(binary_tree_t));
-	if (left_child == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	left_child->n = value;
-	left_child->parent = parent;
+	left_child = binary_tree_node(parent, value);
+	left_child->right = NULL;
 
-	if (parent->left != NULL)
+	if (parent->left)
 	{
 		left_child->left = parent->left;
 		parent->left->parent = left_child;
-		parent->left = left_child;
 	}
-	else
-	{
-		parent->left = left_child;
-	}
-
+	parent->left = left_child;
 	return (left_child);
 }
