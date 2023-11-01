d4e910d6f446694346d0a2cda3111ab4d54570eb
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-11-01 01:37:07 -0700

    check height of a tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -24,7 +24,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			cur = cur->right;
 		}
 
-		return (left_height > right_height ? left_height : right_height);
+		if (left_height > right_height)
+			return (left_height);
+		else
+			return (right_height);
 	}
 	return (0);
 }
