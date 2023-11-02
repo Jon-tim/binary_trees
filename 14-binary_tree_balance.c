177f39b376ec5068a415b093a0cf603faf5c50b3
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-11-02 00:49:07 -0700

    check the balance factor of a tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,19 +7,43 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (tree)
+	{
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_height -  a function that measures the height of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the height.
+ * Return: height or 0, If tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
 	size_t left_height = 0, right_height = 0;
 
 	if (tree)
 	{
 		if (tree->left)
 		{
-			left_height = binary_tree_balance(tree->left) + 1;
+			left_height = binary_tree_height(tree->left) + 1;
 		}
+		else
+		{
+			left_height = 1;
+		}
+
 		if (tree->right)
 		{
-			right_height = binary_tree_balance(tree->right) + 1;
+			right_height = binary_tree_height(tree->right) + 1;
 		}
-		return (left_height - right_height);
+		else
+		{
+			right_height = 1;
+		}
+
+		return ((left_height > right_height ? left_height : right_height));
 	}
 	return (0);
 }
